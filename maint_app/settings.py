a90de5659dc5716aba1521b15e4b3bfe9ee045f9
ethan wendt <ethan.wendt@fieldai.com>
2025-09-03 12:08:38 -0700

    Fix STATIC_URL

--- maint_app/settings.py
+++ maint_app/settings.py
@@ -143,8 +143,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [BASE_DIR / "static"]  
 
 STORAGES = {
      "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"}
