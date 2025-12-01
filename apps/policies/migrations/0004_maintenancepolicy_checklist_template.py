4c7ef82cbe5d22faf535fcbf12440ed9df2c1aa3
ethan wendt <ethan.wendt@fieldai.com>
2025-12-01 09:20:50 -0800

    Fix policies migration dependency on checklist 0008

--- apps/policies/migrations/0004_maintenancepolicy_checklist_template.py
+++ apps/policies/migrations/0004_maintenancepolicy_checklist_template.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('checklists', '0009_remove_checklistrun_created_at_checklistitem_section_and_more'),
+        ('checklists', '0008_pg_idempotent_cleanup'),
         ('policies', '0003_alter_maintenancepolicy_counter_and_more'),
     ]
 
