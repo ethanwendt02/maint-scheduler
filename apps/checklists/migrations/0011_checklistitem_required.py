a44a85831d0556f6fc3f02badc871f12d5787a52
ethan.wendt <ethan.wendt@fieldai.com>
2026-01-19 13:52:50 -0800

    PDF Update

--- apps/checklists/migrations/0011_checklistitem_required.py
+++ apps/checklists/migrations/0011_checklistitem_required.py
@@ -7,4 +7,17 @@ class Migration(migrations.Migration):
         ("checklists", "0010_checklistrun_completed_pdf"),
     ]
 
-    operations = []
+    operations = [
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                # DB already has the column in production, so do nothing here.
+            ],
+            state_operations=[
+                migrations.AddField(
+                    model_name="checklistitem",
+                    name="required",
+                    field=models.BooleanField(default=True),
+                ),
+            ],
+        ),
+    ]
